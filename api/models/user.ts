86d50c14b0974cb6161776ec2d9dcb22f16f4934
FME849 <1414439@gmail.com>
2024-09-11 17:11:15 +0700

    feat: refactor User collection interaction methods

--- api/models/user.ts
+++ api/models/user.ts
@@ -1,9 +1,7 @@
-import { model } from 'mongoose';
+import { model, Schema } from 'mongoose';
 import { generateReferralCode } from 'utils/referral';
 
-import { createSchema } from './utils';
-
-const userSchema = createSchema({
+const userSchema = new Schema({
 	/** bindingId is used to bind this user with dynamodb profile (will be deprecated soon) */
 	bindingId: {
 		type: String,
@@ -12,6 +10,7 @@ const userSchema = createSchema({
 	},
 	points: {
 		type: Number,
+		min: 0,
 		default: 0,
 	},
 	referralCode: {
@@ -24,19 +23,20 @@ const userSchema = createSchema({
 export const User = model('User', userSchema);
 
 export const getOrCreateUserByBindingId = async (bindingId: string) => {
-	const query = User.where({ bindingId });
-	const user = await query.findOne(query);
-
+	const user = await User.findOne({ bindingId }).exec();
 	if (user) {
 		return user;
 	}
-
-	const referralCode = generateReferralCode(7);
-	const newUser = await User.create({ bindingId, referralCode });
-	return newUser;
+	try {
+		const referralCode = generateReferralCode(7);
+		const newUser = new User({ bindingId, points: 0, referralCode });
+		await newUser.save();
+		return newUser;
+	} catch (err) {
+		throw new Error(`Failed to create new user: ${err}`);
+	}
 };
 
 export const getUserByReferralCode = async (referralCode: string) => {
-	const query = User.where({ referralCode });
-	return await query.findOne();
+	return await User.findOne({ referralCode });
 };
--- api/user/index.ts
+++ api/user/index.ts
@@ -2,13 +2,17 @@ import { getItem } from 'aws/dynamo';
 import { getOrCreateUserByBindingId } from 'models/user';
 import type { Profile, QueryResolvers } from 'types/graphql';
 
-const getUser: QueryResolvers['profile'] = async (root, _, { user }) => {
-	const { Item: profile } = await getItem(`profile#${user.id}`);
-	const mongoProfile = await getOrCreateUserByBindingId(user.id);
-	Object.assign(profile, mongoProfile);
-	return profile as Profile;
+const profile: QueryResolvers['profile'] = async (root, _, { user }) => {
+	try {
+		const { Item: profile } = await getItem(`profile#${user.id}`);
+		const mongoProfile = await getOrCreateUserByBindingId(user.id);
+		Object.assign(profile, mongoProfile);
+		return profile as Profile;
+	} catch (err) {
+		throw new Error(err);
+	}
 };
 export const UserQueryResolver = {
-	getUser,
+	profile,
 };
 export const UserMutationResolver = {};
