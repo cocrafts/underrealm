05230dcf8da9c1bf5d386c04fbf370e5f6bf5409
ltminhthu <ltminhthu.209@gmail.com>
2024-09-17 17:07:03 +0700

    adjust responsive approach

--- launcher/screens/Quest/QuestContent.tsx
+++ launcher/screens/Quest/QuestContent.tsx
@@ -1,8 +1,8 @@
 import type { FC } from 'react';
-import { useEffect, useMemo, useState } from 'react';
-import { ActivityIndicator, Image, StyleSheet, View } from 'react-native';
-import type { DimensionState } from '@metacraft/ui';
-import { dimensionState, Text } from '@metacraft/ui';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, Image, View } from 'react-native';
+import { createStyleSheet, useStyles } from 'react-native-unistyles';
+import { Text } from '@metacraft/ui';
 import resources from 'utils/resources';
 import { questActions, questState } from 'utils/state/social/internal';
 import { useSnapshot } from 'valtio';
@@ -12,34 +12,9 @@ import QuestItem from './QuestItem';
 import TabSelection from './TabSelection';
 
 const QuestContent: FC = () => {
-	const { windowSize, isMobile } = useSnapshot<DimensionState>(dimensionState);
 	const { quests, activeDoneQuests } = useSnapshot(questState);
 	const [isLoading, setIsLoading] = useState(true);
-
-	const frameCharmStyle = useMemo(() => {
-		return {
-			width: (windowSize.width * 400) / 1440,
-			height: (windowSize.height * 20) / 1362,
-			marginTop: -(windowSize.height * 20) / 1362,
-		};
-	}, [windowSize]);
-
-	const containerStyle = useMemo(() => {
-		const widthRatio = isMobile ? 203 / 215 : 5 / 6;
-		const heightRatio = isMobile ? 1 / 12 : 60 / 227;
-		return {
-			marginTop: windowSize.height * heightRatio,
-			width: windowSize.width * widthRatio,
-		};
-	}, [windowSize, isMobile]);
-
-	const titleCharmStyle = useMemo(() => {
-		return {
-			width: (windowSize.width * 400) / 1440,
-			height: (windowSize.height * 13) / 1024,
-			bottom: -(windowSize.height * 13) / 1024,
-		};
-	}, [windowSize]);
+	const { styles } = useStyles(stylesheet);
 
 	useEffect(() => {
 		const getQuestData = async () => {
@@ -51,18 +26,12 @@ const QuestContent: FC = () => {
 	}, []);
 
 	return (
-		<View style={[styles.container, containerStyle]}>
-			<Image
-				style={[styles.frameCharm, frameCharmStyle]}
-				source={resources.quest.charm}
-			/>
+		<View style={[styles.container]}>
+			<Image style={styles.frameCharm} source={resources.quest.charm} />
 
 			<View style={styles.titleContainer}>
 				<Text style={styles.title}>Quest To Conquer</Text>
-				<Image
-					source={resources.quest.titleCharm}
-					style={[titleCharmStyle, styles.titleCharm]}
-				/>
+				<Image source={resources.quest.titleCharm} style={styles.titleCharm} />
 			</View>
 
 			<View style={styles.tabsContainer}>
@@ -73,7 +42,7 @@ const QuestContent: FC = () => {
 			{isLoading ? (
 				<ActivityIndicator />
 			) : (
-				<View style={[styles.quests, isMobile ? styles.questsOnMobile : {}]}>
+				<View style={[styles.quests]}>
 					{quests.map((quest) => {
 						const isDone = Array.from(activeDoneQuests).some(
 							(doneQuest) => doneQuest.id === quest.id,
@@ -103,53 +72,60 @@ const QuestContent: FC = () => {
 
 export default QuestContent;
 
-const styles = StyleSheet.create({
-	frameCharm: {
-		alignSelf: 'center',
-	},
-	imageBackground: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	container: {
-		borderColor: '#9F835F',
-		borderWidth: 1,
-		alignItems: 'stretch',
-		paddingBottom: 80,
-		marginBottom: 40,
-		marginTop: 80,
-	},
-	title: {
-		fontFamily: 'Volkhov',
-		color: '#ffffff',
-		fontSize: 22,
-	},
-	titleCharm: {
-		position: 'absolute',
-	},
-	mapDescription: {
-		marginBottom: 60,
-		paddingHorizontal: 15,
-	},
-	quests: {
-		alignItems: 'stretch',
-		paddingHorizontal: 40,
-		marginTop: 40,
-		gap: 16,
-	},
-	questsOnMobile: {
-		paddingHorizontal: 12,
-	},
-	titleContainer: {
-		paddingVertical: 20,
-		alignItems: 'center',
-		alignSelf: 'center',
-	},
-	tabsContainer: {
-		flexDirection: 'row',
-		borderBottomColor: '#2E2E2E',
-		borderBottomWidth: 1,
-		justifyContent: 'center',
-	},
+const stylesheet = createStyleSheet(() => {
+	return {
+		frameCharm: {
+			alignSelf: 'center',
+			width: { xs: 300, lg: 400 },
+			height: { xs: 24, lg: 30 },
+			marginTop: { xs: -16, lg: -20 },
+		},
+		imageBackground: {
+			flex: 1,
+			alignItems: 'center',
+			justifyContent: 'center',
+		},
+		container: {
+			borderColor: '#9F835F',
+			borderWidth: 1,
+			alignItems: 'stretch',
+			paddingBottom: 80,
+			paddingHorizontal: { xs: 16, md: 20, lg: 40 },
+			marginBottom: 40,
+			marginTop: 80,
+			alignSelf: 'stretch',
+		},
+		title: {
+			fontFamily: 'Volkhov',
+			color: '#ffffff',
+			fontSize: 22,
+		},
+		titleCharm: {
+			position: 'absolute',
+			width: 400,
+			height: 13,
+			bottom: -13,
+		},
+		mapDescription: {
+			marginBottom: 60,
+			paddingHorizontal: 15,
+		},
+		quests: {
+			alignItems: 'stretch',
+			paddingHorizontal: { xs: 12, md: 40 },
+			marginTop: 40,
+			gap: 16,
+		},
+		titleContainer: {
+			paddingVertical: 20,
+			alignItems: 'center',
+			alignSelf: 'center',
+		},
+		tabsContainer: {
+			flexDirection: 'row',
+			borderBottomColor: '#2E2E2E',
+			borderBottomWidth: 1,
+			justifyContent: 'center',
+		},
+	};
 });
--- launcher/screens/Quest/index.tsx
+++ launcher/screens/Quest/index.tsx
@@ -1,40 +1,20 @@
 import type { FC } from 'react';
-import { useMemo } from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
-import type { DimensionState } from '@metacraft/ui';
-import { dimensionState } from '@metacraft/ui';
+import { ImageBackground } from 'react-native';
+import { createStyleSheet, useStyles } from 'react-native-unistyles';
 import ScrollLayout from 'components/layouts/Scroll';
 import resources from 'utils/resources';
-import { useSnapshot } from 'valtio';
 
 import HeadingSection from './HeadingSection';
 import QuestContent from './QuestContent';
 
 const QuestScreen: FC = () => {
-	const { windowSize, isMobile } = useSnapshot<DimensionState>(dimensionState);
-
-	const imageHeight = useMemo(
-		() => (windowSize.width * 1362) / 1440,
-		[windowSize],
-	);
-
-	const imageBackgroundStyle = useMemo(() => {
-		if (isMobile)
-			return {
-				height: imageHeight,
-			};
-
-		return {
-			height: imageHeight,
-			paddingTop: (imageHeight * 140) / 1024,
-		};
-	}, [windowSize, isMobile]);
+	const { styles } = useStyles(stylesheet);
 
 	return (
 		<ScrollLayout style={styles.container}>
 			<ImageBackground
 				source={resources.quest.headingBackground}
-				style={[styles.imageBackground, imageBackgroundStyle]}
+				style={styles.imageBackground}
 			>
 				<HeadingSection />
 				<QuestContent />
@@ -45,17 +25,20 @@ const QuestScreen: FC = () => {
 
 export default QuestScreen;
 
-const styles = StyleSheet.create({
-	container: {
-		backgroundColor: '#190E0E',
-	},
-	frameCharm: {
-		alignSelf: 'center',
-	},
-	imageBackground: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-		paddingTop: 80,
-	},
+const stylesheet = createStyleSheet(() => {
+	return {
+		container: {
+			backgroundColor: '#190E0E',
+		},
+		frameCharm: {
+			alignSelf: 'center',
+		},
+		imageBackground: {
+			flex: 1,
+			alignItems: 'center',
+			justifyContent: 'center',
+			alignSelf: 'stretch',
+			paddingHorizontal: { xs: 16, sm: 20, md: 40, lg: 80, xl: 120 },
+		},
+	};
 });
