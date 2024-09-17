b7e4362d50a0321b95d41163ec0d6ecd6e76a2ad
Tan Le <tanlethanh8266@gmail.com>
2024-09-17 15:57:01 +0700

    config: setup test with jest

---
+++ api/social/mutation/quest.test.ts
@@ -0,0 +1,5 @@
+describe('Test Quest mutations', () => {
+	it('Complete quest with adding points', () => {});
+	it('Complete done quest failed', () => {});
+	it('Complete quest failed when running concurrently', () => {});
+});
