4fb5e0ad131f147e7ae588c50a5e4229f7b14e93
Tan Le <tanlethanh8266@gmail.com>
2024-09-18 12:19:31 +0700

    fix: fully generic inferred types from createSchema

--- api/models/utils.ts
+++ api/models/utils.ts
@@ -1,33 +1,21 @@
-import type { SchemaDefinition, SchemaDefinitionType, Types } from 'mongoose';
+import type { Types } from 'mongoose';
 import { Schema } from 'mongoose';
-
-type SchemaParams = ConstructorParameters<typeof Schema>;
-
-type Definition<RawDocType> = SchemaDefinition<
-	SchemaDefinitionType<RawDocType>,
-	RawDocType
->;
-
-type Options = SchemaParams[1];
 
 /**
  * a wrapper for `new Schema()` with:
  * - virtual `id` from default `_id objectId` for GraphQL field compatibility
  * - auto `timestamps` on creating or updating
  */
-export const createSchema = <RawDocType>(
-	definition: Definition<RawDocType> | RawDocType,
+export const createSchema = <Definition extends {}, Options>(
+	definition: Definition,
 	options?: Options,
 ) => {
-	const defaultOptions: Options = {
+	const schema = new Schema(definition, {
 		toJSON: { virtuals: true },
 		toObject: { virtuals: true },
 		timestamps: true,
-	};
-
-	const mergedOption = { ...defaultOptions, ...options };
-
-	const schema = new Schema(definition, mergedOption);
+		...options,
+	});
 
 	schema.virtual('id').get(function () {
 		return (this._id as Types.ObjectId).toHexString();
