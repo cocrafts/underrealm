ac134ba46123648dc4cea22e2b7585fb3fc95266
FME849 <1414439@gmail.com>
2024-09-18 16:33:09 +0700

    feat: refactor referral mutation

--- api/models/referral.ts
+++ api/models/referral.ts
@@ -1,7 +1,5 @@
 import { model } from 'mongoose';
-import { ClientError } from 'utils/errors';
 
-import { User } from './user';
 import { createSchema } from './utils';
 
 const referralSchema = createSchema({
@@ -17,34 +15,3 @@ const referralSchema = createSchema({
 referralSchema.index({ referrerId: 1, refereeId: 1 }, { unique: true });
 
 export const Referral = model('Referral', referralSchema);
-
-export const createReferralRecord = async (
-	refereeId: string,
-	referralCode: string,
-) => {
-	const referrer = await User.findOne({ referralCode });
-	if (!referrer?.id) {
-		throw new ClientError('Referral code is invalid');
-	}
-
-	const existReferral = await Referral.findOne({
-		referrerId: referrer.id,
-		refereeId,
-	});
-	if (existReferral.id) {
-		throw new ClientError(
-			'Can not make new referral cause referral already existed',
-		);
-	}
-
-	Referral.create({
-		referrerId: referrer.id,
-		refereeId,
-		claimedPoints: 80,
-	});
-	User.findOneAndUpdate(
-		{ bindingId: referrer.id },
-		{ points: referrer.points + 80 },
-	);
-	return true;
-};
--- api/social/mutation/referral.ts
+++ api/social/mutation/referral.ts
@@ -1,12 +1,38 @@
-import { createReferralRecord } from 'models/referral';
+import { Referral } from 'models/referral';
+import { User } from 'models/user';
 import type { MutationResolvers } from 'types/graphql';
+import { ClientError } from 'utils/errors';
 
 export const makeReferral: MutationResolvers['makeReferral'] = async (
 	root,
 	{ referralCode },
 	{ user },
 ) => {
-	const result = await createReferralRecord(user.id, referralCode);
+	const referrer = await User.findOne({ referralCode });
+	if (!referrer?.id) {
+		throw new ClientError('Referral code is invalid');
+	}
 
-	return result;
+	const existReferral = await Referral.findOne({
+		referrerId: referrer.id,
+		refereeId: user.id,
+	});
+	if (existReferral.id) {
+		throw new ClientError(
+			'Can not make new referral cause referral already existed',
+		);
+	}
+
+	const referralPoints = 80;
+	Referral.create({
+		referrerId: referrer.id,
+		refereeId: user.id,
+		claimedPoints: referralPoints,
+	});
+	User.findOneAndUpdate(
+		{ bindingId: referrer.id },
+		{ points: referrer.points + referralPoints },
+	);
+
+	return true;
 };
