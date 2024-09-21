14ed0b763de935d7586977be466a3e8564e9c5a0
Tan Le <tanlethanh8266@gmail.com>
2024-09-21 20:36:41 +0700

    chore: config logger for deployment

--- api/functions/graphql.ts
+++ api/functions/graphql.ts
@@ -2,11 +2,14 @@ import {
 	handlers,
 	startServerAndCreateLambdaHandler,
 } from '@as-integrations/aws-lambda';
+import { logger } from 'utils/logger';
 import { graphqlContext } from 'utils/runtime';
 
 import '../models';
 
 import { apolloServer } from '../apollo';
+
+logger.info('API is running');
 
 export const handler = startServerAndCreateLambdaHandler(
 	apolloServer,
