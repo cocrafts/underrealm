b295a936f325fbb524131fe9d857d175c11ec996
Tan Le <tanlethanh8266@gmail.com>
2024-09-22 00:40:33 +0700

    chore: bypass introspection request

--- api/utils/context/index.ts
+++ api/utils/context/index.ts
@@ -1,8 +1,7 @@
 import type { ContextFunction } from '@apollo/server';
 import { type IUser, User } from 'models/user';
 import { generateRandomCode, REFERRAL_CODE_LENGTH } from 'utils/common';
-
-import { UnauthorizedError } from '../errors';
+import { ForbiddenError } from 'utils/errors';
 
 import { verifyToken } from './jwt';
 
@@ -18,22 +17,25 @@ export const graphqlContext: ContextFunction<unknown[], ApiContext> = async ({
 }) => {
 	const headers = event?.headers || req?.headers || {};
 	const auth: string = headers['Authorization'] || headers['authorization'];
+	const isIntrospection = req?.body.query.includes('Introspection');
 
-	if (!auth) throw new UnauthorizedError('require Authorization token');
+	if (auth) {
+		const token = auth.replace('Bearer ', '');
+		const cognitoUser = await verifyToken(token);
 
-	const token = auth.replace('Bearer ', '');
-	const cognitoUser = await verifyToken(token);
+		let user: IUser = await User.findOne({ bindingId: cognitoUser.username });
 
-	let user: IUser = await User.findOne({ bindingId: cognitoUser.username });
+		if (!user) {
+			user = await User.create({
+				bindingId: cognitoUser.username,
+				address: cognitoUser.wallet,
+				email: cognitoUser.email,
+				referralCode: generateRandomCode(REFERRAL_CODE_LENGTH),
+			});
+		}
 
-	if (!user) {
-		user = await User.create({
-			bindingId: cognitoUser.username,
-			address: cognitoUser.wallet,
-			email: cognitoUser.email,
-			referralCode: generateRandomCode(REFERRAL_CODE_LENGTH),
-		});
+		return { user };
+	} else if (!isIntrospection) {
+		throw new ForbiddenError('Require Authorization token');
 	}
-
-	return { user };
 };
