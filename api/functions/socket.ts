57f81e96a31ea04878c1eaf419f0e4a6abcb63e4
Tan Le <tanlethanh8266@gmail.com>
2024-09-23 16:27:47 +0700

    feat(wip): websocket to lambda runtime with aws api gateway

--- api/functions/socket.ts
+++ api/functions/socket.ts
@@ -1,16 +1,54 @@
-import type { APIGatewayProxyWebsocketHandlerV2 } from 'aws-lambda';
+import type {
+	APIGatewayProxyStructuredResultV2,
+	APIGatewayProxyWebsocketHandlerV2,
+} from 'aws-lambda';
+import { parse, subscribe, validate } from 'graphql';
 import { StatusCodes } from 'http-status-codes';
+import mongoose from 'mongoose';
+import { postToConnection } from 'utils/aws/gateway';
+import { configs } from 'utils/config';
+import { globalContext } from 'utils/context/index.lambda';
+import { logger } from 'utils/logger';
+import { redis } from 'utils/redis';
 
-export const handler: APIGatewayProxyWebsocketHandlerV2 = async (event) => {
+import { schema } from '../apollo';
+
+const initPromises = [redis.connect(), mongoose.connect(configs.MONGO_URI)];
+
+export const handler: APIGatewayProxyWebsocketHandlerV2 = async (
+	event,
+	context,
+	callback,
+) => {
+	await Promise.all(initPromises);
+
 	switch (event.requestContext.routeKey) {
 		case '$connect': {
-			return { statusCode: StatusCodes.OK };
+			logger.info('connect', event.requestContext);
+			return ok();
 		}
 		case '$disconnect': {
-			return { statusCode: StatusCodes.OK };
+			logger.info('disconnect', event.requestContext);
+			return ok();
 		}
-		case 'graphql': {
-			return { statusCode: StatusCodes.OK };
+		case '$default': {
+			const connectionId = event.requestContext.connectionId;
+
+			try {
+				const result = await handleGraphqlSubscription(
+					event,
+					context,
+					callback,
+				);
+				if (result) return result;
+			} catch (error) {
+				await postToConnection(connectionId, {
+					payload: { errors: [(error as Error).message] },
+					type: 'error',
+				});
+			}
+
+			break;
 		}
 		default: {
 			return {
@@ -21,4 +59,61 @@ export const handler: APIGatewayProxyWebsocketHandlerV2 = async (event) => {
 			};
 		}
 	}
+
+	return ok();
+};
+
+const handleGraphqlSubscription: APIGatewayProxyWebsocketHandlerV2 = async (
+	event,
+) => {
+	logger.info('Handle event from', event.requestContext);
+	const connectionId = event.requestContext.connectionId;
+	globalContext.connectionId = connectionId;
+
+	if (!event.body) return ok();
+
+	const operation = JSON.parse(event.body);
+
+	if (operation.type === 'connection_init') {
+		await postToConnection(connectionId, { type: 'connection_ack' });
+		return ok();
+	} else if (operation.type === 'stop') {
+		return ok();
+	}
+
+	const { query, variables, operationName } = operation.payload;
+	const document = parse(query);
+	const validationErrors = validate(schema, document);
+	if (validationErrors.length > 0) {
+		await postToConnection(connectionId, {
+			payload: { errors: validationErrors },
+			type: 'error',
+		});
+		return ok();
+	}
+
+	try {
+		await subscribe({
+			document,
+			schema,
+			rootValue: {},
+			operationName: operationName,
+			variableValues: variables,
+			contextValue: {},
+		});
+	} catch (err) {
+		await postToConnection(connectionId, {
+			id: operation.id,
+			payload: err,
+			type: 'error',
+		});
+	}
+
+	return ok();
+};
+
+const ok = (
+	result?: Omit<APIGatewayProxyStructuredResultV2, 'statusCode'>,
+): APIGatewayProxyStructuredResultV2 => {
+	return { ...result, statusCode: StatusCodes.OK };
 };
--- api/utils/pubsub/index.lambda.ts
+++ api/utils/pubsub/index.lambda.ts
@@ -1,27 +1,66 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { PubSubEngine } from 'graphql-subscriptions';
+import { type PubSubEngine } from 'graphql-subscriptions';
+import { postToConnection } from 'utils/aws/gateway';
+import { globalContext } from 'utils/context/index.lambda';
+import { logger } from 'utils/logger';
+import { redis } from 'utils/redis';
 
 export * from './utils';
 
-export class LambdaPubsub implements PubSubEngine {
+class LambdaPubsub implements PubSubEngine {
 	public asyncIterator<T>(triggers: string | string[]): AsyncIterable<T> {
-		console.log(triggers);
-		return {} as never;
+		return {
+			[Symbol.asyncIterator]: () => {
+				return {
+					next: async () => {
+						if (typeof triggers === 'string') {
+							await this.registerTopic(triggers, globalContext.connectionId);
+						} else {
+							const registerPromises = triggers.map(async (topic) => {
+								await this.registerTopic(topic, globalContext.connectionId);
+							});
+
+							await Promise.all(registerPromises);
+						}
+
+						return Promise.resolve({ done: true, value: null });
+					},
+				};
+			},
+		};
 	}
 
 	public async publish(triggerName: string, payload: any): Promise<void> {
-		console.log(triggerName, payload);
+		const connectionIds = await redis.SMEMBERS(triggerName);
+
+		const sendPromises = connectionIds.map(async (cid) => {
+			return await postToConnection(cid, payload);
+		});
+
+		await Promise.all(sendPromises);
 	}
 
-	public async subscribe(
-		triggerName: string,
-		onMessage: (...args: any[]) => void,
-	): Promise<number> {
-		console.log(triggerName, onMessage);
+	public async subscribe(triggerName: string, onMessage: any): Promise<number> {
+		if (onMessage) {
+			logger.warn('onMessage is ignored by Lambda pubsub');
+		}
+		await this.registerTopic(triggerName, globalContext.connectionId);
+
 		return 0;
 	}
 
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	public unsubscribe(subId: number) {
-		console.log(subId);
+		throw Error('unsubscribe is not supported in lambda runtime');
+	}
+
+	private async registerTopic(topic: string, connectionId: string) {
+		const topicKey = `pubsub:topic#${topic}`;
+		await redis.SADD(topicKey, connectionId);
+		await redis.EXPIRE(topicKey, 60 * 60 * 24);
+		const connectionKey = `pubsub:connection#${connectionId}`;
+		await redis.SADD(connectionKey, topicKey);
 	}
 }
+
+export const pubsub = new LambdaPubsub();
