8d8afcd0eaffa02bd399e7d65d5fa13b167171c0
Tan Le <tanlethanh8266@gmail.com>
2024-09-23 14:10:45 +0700

    fix: wait for init middleware to resolve all connections

--- api/functions/graphql.ts
+++ api/functions/graphql.ts
@@ -2,8 +2,11 @@ import {
 	handlers,
 	startServerAndCreateLambdaHandler,
 } from '@as-integrations/aws-lambda';
-import { graphqlContext } from 'utils/context';
+import mongoose from 'mongoose';
+import { configs } from 'utils/config';
+import { graphqlContext } from 'utils/context/index.lambda';
 import { logger } from 'utils/logger';
+import { redis } from 'utils/redis';
 
 import '../models';
 
@@ -11,8 +14,16 @@ import { apolloServer } from '../apollo';
 
 logger.info('API is running');
 
+const initPromises = [redis.connect(), mongoose.connect(configs.MONGO_URI)];
+
+const requestHandler = handlers.createAPIGatewayProxyEventV2RequestHandler();
+
+const waitForInit = async () => {
+	await Promise.all(initPromises);
+};
+
 export const handler = startServerAndCreateLambdaHandler(
 	apolloServer,
-	handlers.createAPIGatewayProxyEventV2RequestHandler(),
-	{ context: graphqlContext },
+	requestHandler,
+	{ context: graphqlContext, middleware: [waitForInit] },
 );
