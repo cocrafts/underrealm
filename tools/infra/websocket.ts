322a983096cc0aa08a3de5d9f1482edc71a33231
Tan Le <tanlethanh8266@gmail.com>
2024-09-25 20:43:25 +0700

    chore: no more construct function to easily link between resources

--- tools/infra/websocket.ts
+++ tools/infra/websocket.ts
@@ -1,41 +1,39 @@
 import dotenv from 'dotenv';
 
 import {
-	APIEnvs,
 	constructDomainName,
+	DBEnvs,
 	defaultEnvs,
 	defaultLambdaConfigs,
 	zoneId,
 } from './shared';
 
-export const constructWebsocketAPI = () => {
-	dotenv.config({ path: `api/.env.${$app.stage}` });
+dotenv.config({ path: `api/.env.${$app.stage}` });
 
-	const domainName = constructDomainName('websocket', $app.stage);
+const domainName = constructDomainName('websocket', $app.stage);
 
-	const wsAPI = new sst.aws.ApiGatewayWebSocket('websocket', {
-		domain: {
-			name: domainName,
-			dns: sst.aws.dns({ zone: zoneId }),
+const wsAPI = new sst.aws.ApiGatewayWebSocket('websocket', {
+	domain: {
+		name: domainName,
+		dns: sst.aws.dns({ zone: zoneId }),
+	},
+});
+
+const socket = new sst.aws.Function('socket', {
+	...defaultLambdaConfigs($app.stage),
+	handler: 'api/functions/socket.handler',
+	copyFiles: [{ from: 'api/schema.graphql', to: 'schema.graphql' }],
+	environment: { ...defaultEnvs(), ...DBEnvs() },
+	permissions: [
+		{
+			actions: ['execute-api:Invoke', 'execute-api:ManageConnections'],
+			resources: [wsAPI.nodes.api.executionArn.apply((t) => `${t}/**/*`)],
 		},
-	});
+	],
+});
 
-	const socket = new sst.aws.Function('socket', {
-		...defaultLambdaConfigs($app.stage),
-		handler: 'api/functions/socket.handler',
-		copyFiles: [{ from: 'api/schema.graphql', to: 'schema.graphql' }],
-		environment: { ...defaultEnvs(), ...APIEnvs() },
-		permissions: [
-			{
-				actions: ['execute-api:Invoke', 'execute-api:ManageConnections'],
-				resources: [wsAPI.nodes.api.executionArn.apply((t) => `${t}/**/*`)],
-			},
-		],
-	});
+wsAPI.route('$connect', socket.arn as never);
+wsAPI.route('$disconnect', socket.arn as never);
+wsAPI.route('$default', socket.arn as never);
 
-	wsAPI.route('$connect', socket.arn as never);
-	wsAPI.route('$disconnect', socket.arn as never);
-	wsAPI.route('$default', socket.arn as never);
-
-	return wsAPI;
-};
+export { wsAPI };
