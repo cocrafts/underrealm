04fbc5c8a79d4dfd2e93d6f0cdee074c1f5817e4
FME849 <1414439@gmail.com>
2024-09-26 11:12:01 +0700

    chore: fix referral not auto-fill after sign-in

--- launcher/utils/referral.ts
+++ launcher/utils/referral.ts
@@ -3,6 +3,7 @@ const localStorageKey = 'REFERRAL_CODE';
 export const extractReferralFromUrl = () => {
 	const params = new URL(window.location.toString()).searchParams;
 	const referralCode = params.get('ref');
+	if (!referralCode || referralCode === 'null') return;
 	window.localStorage.setItem(localStorageKey, referralCode);
 };
 
