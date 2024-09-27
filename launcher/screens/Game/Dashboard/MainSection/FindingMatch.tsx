206a644e96cd28d7649861aeb3eef3c6867c4a64
Tan Le <tanlethanh8266@gmail.com>
2024-09-27 14:20:26 +0700

    feat: navigate to game after match found

--- launcher/screens/Game/Dashboard/MainSection/FindingMatch.tsx
+++ launcher/screens/Game/Dashboard/MainSection/FindingMatch.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from '@metacraft/ui';
 import Loading from 'components/Loading';
@@ -11,12 +12,14 @@ export const FindingMatch = () => {
 		variables: { userId: profile.id },
 	});
 
-	if (data) {
-		navigate('Game', {
-			screen: 'Duel',
-			params: { id: data.findMatch?.id },
-		});
-	}
+	useEffect(() => {
+		if (data) {
+			console.log('Match found', data.findMatch.id);
+			localStorage.setItem('GAME_JWT', data.findMatch.jwt);
+			localStorage.setItem('GAME_WS_URI', SOCKET_URI);
+			navigate('Game', { screen: 'Duel', params: { id: data.findMatch.id } });
+		}
+	}, [data]);
 
 	if (loading) return <Loading />;
 	return <Text style={styles.title}>Match found</Text>;
