f2f569ee26c66989f1f35b8bbf2b2fc4f942e46e
FME849 <1414439@gmail.com>
2024-10-07 09:25:22 +0700

    feat: impl fastReplay

--- game/assets/scripts/replay/index.ts
+++ game/assets/scripts/replay/index.ts
@@ -10,10 +10,57 @@ import { playDraw } from './draw';
 import { playFight } from './fight';
 import { playGeneric } from './generic';
 import { playReinforce } from './reinforce';
+import { replicateState } from './replicateState';
 import { playSummon } from './summon';
 
 let replaying = false;
 const { BundleGroup, mergeFragmentToState, runCommand, move } = Engine;
+
+export const fastReplay = async (): Promise<void> => {
+	const remoteHistoryLength = system.history.length;
+	const isUpToDate = system.historyLevel >= remoteHistoryLength;
+
+	if (system.winner || replaying || isUpToDate) return;
+
+	replaying = true;
+
+	for (let i = system.historyLevel; i < remoteHistoryLength; i += 1) {
+		const bundle = system.history[i];
+		const group = bundle?.group;
+		const isInitialDraw = group === BundleGroup.InitialDraw;
+		const isTurnDraw = group === BundleGroup.TurnDraw;
+		const isDraw = isInitialDraw || isTurnDraw;
+		const isMyPhase = bundle.phaseOf === system.playerIds.me;
+
+		runCommandBundle(bundle);
+
+		if (!system.winner && isTurnDraw && isMyPhase) {
+			await showTurnRibbon('Your Turn');
+		}
+
+		if (i >= remoteHistoryLength - 3) {
+			if (isDraw) {
+				await playDraw(bundle);
+			} else if (BundleGroup.Summon === group) {
+				await playSummon(bundle);
+			} else if (BundleGroup.FightCombat === group) {
+				await playFight(bundle);
+			} else if (BundleGroup.Reinforce === group) {
+				await playReinforce(bundle);
+			} else {
+				await playGeneric(bundle);
+			}
+		} else if (i === remoteHistoryLength - 4) {
+			replicateState();
+		}
+
+		updatePlayers();
+		updateGroundUnits();
+		system.historyLevel += 1;
+	}
+
+	replaying = false;
+};
 
 export const replay = async (): Promise<void> => {
 	const remoteHistoryLength = system.history.length;
