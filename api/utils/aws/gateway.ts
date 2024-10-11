5cb54c1233ac9082f8f6c6845bd1d387da7a55ff
Tan Le <tanlethanh8266@gmail.com>
2024-10-11 15:15:52 +0700

    chore: ignore GoneException error

--- api/utils/aws/gateway.ts
+++ api/utils/aws/gateway.ts
@@ -3,6 +3,7 @@ import {
 	PostToConnectionCommand,
 } from '@aws-sdk/client-apigatewaymanagementapi';
 import { configs } from 'utils/config';
+import { logger } from 'utils/logger';
 
 import { wsEndpoint } from './utils';
 
@@ -13,10 +14,19 @@ export const wsClient = new ApiGatewayManagementApiClient({
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const postToConnection = async (connectionId: string, payload: any) => {
-	return await wsClient.send(
-		new PostToConnectionCommand({
-			ConnectionId: connectionId,
-			Data: JSON.stringify(payload),
-		}),
-	);
+	try {
+		return await wsClient.send(
+			new PostToConnectionCommand({
+				ConnectionId: connectionId,
+				Data: JSON.stringify(payload),
+			}),
+		);
+	} catch (error) {
+		// ignore GoneException
+		logger.error('Can not post to connection', {
+			connectionId,
+			payload,
+			error,
+		});
+	}
 };
