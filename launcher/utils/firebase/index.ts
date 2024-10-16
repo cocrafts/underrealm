c29fc66f70aae0f4275af709a5175f0a81e26236
Tan Le <104194494+tanlethanh@users.noreply.github.com>
2024-10-16 15:00:47 +0700

    fix: resolve game cache by using md5 hash build option, reload by checking remote config latestVersion (#57)
    * chore: new game build with md5 cache to prevent scripts cache, add source map
    
    * chore: update build game doc
    
    * feat: firebase remote config with latest version
    
    * chore: use md5 hash scripts for prevent cache
    
    * chore: add doc to remind update script imports

---
+++ launcher/utils/firebase/index.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getRemoteConfig } from 'firebase/remote-config';
+
+const firebaseConfig = {
+	apiKey: FIREBASE_API_KEY,
+	authDomain: FIREBASE_AUTH_DOMAIN,
+	projectId: FIREBASE_PROJECT_ID,
+	storageBucket: FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+	appId: FIREBASE_APP_ID,
+	measurementId: FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const remoteConfig = getRemoteConfig(app);
